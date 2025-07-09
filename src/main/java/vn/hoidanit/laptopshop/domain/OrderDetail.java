53154b1bbb0245dd5d583a30c0f04df8134c90c4
tunglv <tunglv@vtn-global.com>
2025-07-10 00:30:14 +0700

    push code

--- src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/OrderDetail.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.domain;
 
 import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity(name = "oder_detail")
 public class OrderDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
